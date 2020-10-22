4a87060c2ffaf9f3225930b5930da39795503647
Oliver Lohmann <oliver@lohmann.io>
2020-10-22 20:49:10 +0200

    Updated to latest Identity Framework and fixed header handling.

--- src/app/AspNetCoreSampleAppAAD/Startup.cs
+++ src/app/AspNetCoreSampleAppAAD/Startup.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -17,23 +16,27 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
+using Microsoft.Identity.Web;
 
 namespace AspNetCoreSampleAppAAD
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment _env;
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
-                .AddAzureAD(options => Configuration.Bind("AzureAd", options));
+            // Sign-in users with the Microsoft identity platform
+            services.AddMicrosoftIdentityWebAppAuthentication(Configuration);
 
             services.AddControllersWithViews(options =>
             {
@@ -44,46 +47,58 @@ namespace AspNetCoreSampleAppAAD
             });
             services.AddRazorPages();
 
-            services.Configure<ForwardedHeadersOptions>(options =>
+            if (!_env.IsDevelopment())
             {
-                options.ForwardedHeaders =
-                    ForwardedHeaders.XForwardedFor 
-                    | ForwardedHeaders.XForwardedProto 
-                    | ForwardedHeaders.XForwardedHost; // -> Support redirection with xyz.azurefd.net / contoso.com
-            });
+                services.Configure<ForwardedHeadersOptions>(options =>
+                {
+                    options.ForwardedHeaders =
+                        ForwardedHeaders.All;
+
+                    options.RequireHeaderSymmetry = false;
+                    options.ForwardLimit = null;
+
+                    // Azure Front Door
+                    // See: https://docs.microsoft.com/en-us/azure/frontdoor/front-door-faq#how-do-i-lock-down-the-access-to-my-backend-to-only-azure-front-door 
+                    options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("147.243.0.0"), 16));
+                });
+            }
 
             services.AddHealthChecks();
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, ILogger<Startup> logger)
         {
-            // Ensure the "right" Front Door is calling.
-            var expectedFdid = Configuration.GetValue<string>("X-Azure-FDID");
-            if (!string.IsNullOrWhiteSpace(expectedFdid))
+            if (!_env.IsDevelopment())
             {
-                app.Use(async (context, next) =>
+                // Ensure the "right" Front Door is calling.
+                var expectedFdid = Configuration.GetValue<string>("X-Azure-FDID");
+                if (!string.IsNullOrWhiteSpace(expectedFdid))
                 {
-                    context.Request.Headers.TryGetValue("X-Azure-FDID", out var actualFdid);
-                    if (actualFdid == expectedFdid)
+                    app.Use(async (context, next) =>
                     {
-                        await next();
-                    }
-                    else
-                    {
-                        logger.LogWarning("Abort request due to missing X-Azure-FDID", 
-                            context.Connection.RemoteIpAddress);
-                        context.Response.StatusCode = (int) HttpStatusCode.BadGateway;
-                        await context.Response.WriteAsync("Bad Gateway");
-                    }
-                });
-            };
-            
-            app.UseForwardedHeaders();
+                        context.Request.Headers.TryGetValue("X-Azure-FDID", out var actualFdid);
+                        if (actualFdid == expectedFdid)
+                        {
+                            await next();
+                        }
+                        else
+                        {
+                            logger.LogWarning("Abort request due to missing X-Azure-FDID",
+                                context.Connection.RemoteIpAddress);
+                            context.Response.StatusCode = (int) HttpStatusCode.BadGateway;
+                            await context.Response.WriteAsync("Bad Gateway");
+                        }
+                    });
+                }
+                
+                app.UseForwardedHeaders();
+            }
+
             app.UseHealthChecks("/health");
             
-            if (env.IsDevelopment())
+            if (_env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
